8389efde6ce0006b7dc2d903df412ad71d132d72
FredWu <FredWu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-07 06:26:16 +0000

    tagging 1.0b1
    git-svn-id: https://plugins.svn.wordpress.org/syntaxhighlighter-plus/tags/1.0b1@92535 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,170 @@
+=== SyntaxHighlighter Plus ===
+Contributors: matt, Viper007Bond, mdawaffe, FredWu
+Tags: source, code, sourcecode, php, javascript, xhtml, html, css, syntax, highlight, highlighter
+Requires at least: 2.0
+Tested up to: 2.7.x
+Stable tag: trunk
+
+Easily post source code such as PHP or HTML and display it in a styled box.
+
+== Description ==
+
+SyntaxHighlighter Plus is a Wordpress plugin for code syntax highlighting. It is an enhanced version of the original SyntaxHighlighter by Matt, Viper007Bond and mdawaffe. Please give them a big applause for making such a great plugin!
+
+SyntaxHighlighter allows you to easily post syntax highlighted code all without loosing it's formatting or making an manual changes.
+
+It supports the following languages (the alias for use in the post is listed next to the name):
+
+* Bash -- `bash`, `sh`
+* C++ -- `cpp`, `c`, `c++`
+* C# -- `c#`, `c-sharp`, `csharp`
+* CSS -- `css`
+* Delphi -- `delphi`, `pascal`
+* Diff -- `diff`
+* Groovy -- `groovy`
+* Java -- `java`
+* JavaScript -- `js`, `jscript`, `javascript`
+* PHP -- `php`
+* Plain text -- `plain`, `text`
+* Python -- `py`, `python`
+* Ruby -- `rb`, `ruby`, `rails`, `ror`
+* SQL -- `sql`
+* VB -- `vb`, `vb.net`
+* XML/HTML -- `xml`, `html`, `xhtml`, `xslt`
+
+This plugin uses the [SyntaxHighlighter JavaScript package by Alex Gorbatchev](http://code.google.com/p/syntaxhighlighter/).
+
+== Installation ==
+
+###Updgrading From A Previous Version###
+
+To upgrade from a previous version of this plugin, delete the entire folder and files from the previous version of the plugin and then follow the installation instructions below.
+
+* Starting from Wordpress 2.7, plugin upgrades are handled by Wordpress, there is no need to manually upload files.
+
+Important: If you have made any changes to the CSS file, please make sure to back it up before upgrade!
+
+###Uploading The Plugin###
+
+Extract all files from the ZIP file, making sure to keep the file structure intact, and then upload it to `/wp-content/plugins/`.
+
+This should result in the following file structure:
+
+`- wp-content
+    - plugins
+        - syntaxhighlighter-plus
+            | readme.txt
+            | syntaxhighlighter.php
+            - syntaxhighlighter
+                - scripts
+                    | clipboard.swf
+                    | shBrushBash.js
+                    | shBrushCpp.js
+                    | shBrushCSharp.js
+                    | [...]
+                    | shCore.js
+	                | shLegacy.js
+                - src
+                    | shCore.js
+	                | shLegacy.js
+                - styles
+                    | SyntaxHighlighter.css`
+
+**See Also:** ["Installing Plugins" article on the WP Codex](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins)
+
+###Plugin Activation###
+
+Go to the admin area of your WordPress install and click on the "Plugins" menu. Click on "Activate" for the "SyntaxHighlighter" plugin.
+
+###Plugin Usage###
+
+Just wrap your code in `[sourcecode language='css']code here[/sourcecode]`. The language attribute is **required**! See the [plugin's description](http://wordpress.org/extend/plugins/syntaxhighlighter/) for a list of valid language attributes.
+
+== Frequently Asked Questions ==
+
+= The BBCode in my post is being replaced with &lt;pre&gt;'s just fine, but I don't see the syntax highlighting! =
+
+Make sure your theme's footer has `<?php wp_footer(); ?>` somewhere in it, otherwise the JavaScript highlighting files won't be loaded.
+
+= I still see the BBCode in my post. What gives? =
+
+Make sure you correctly use the BBCode with a valid language attribute. A malformed usage of it won't result in replacement.
+
+= I use the visual editor, my code has lots of line breaks inserted, what do I do? =
+
+Use 'shift + return' instead of 'return' when changing lines.
+
+== Screenshots ==
+
+[SyntaxHighlighter Themes](http://alexgorbatchev.com/wiki/SyntaxHighlighter:Themes)
+
+== Other BBCode Methods ==
+
+Find `[sourcecode language='css']code here[/sourcecode]` too long to type? Here's some alternative examples:
+
+* `[source language='css']code here[/source]`
+* `[code language='css']code here[/code]`
+* `[sourcecode lang='css']code here[/sourcecode]`
+* `[source lang='css']code here[/source]`
+* `[code lang='css']code here[/code]`
+* `[sourcecode='css']code here[/sourcecode]`
+* `[source='css']code here[/source]`
+* `[code='css']code here[/code]`
+* `[lang='css']code here[/lang]`
+* `[css]code here[/css]` (or any of the supported language)
+
+Note: Quotation marks around the language can be omitted for even shorter and neater syntax. :-)
+
+== ChangeLog ==
+
+**Version 1.0b1**
+
+* Upgraded the core engine to SyntaxHighlighter 2.0
+* Added configuration: themes
+
+**Version 0.18**
+
+* Added PHP as the default language if no language is specified (e.g. `[code][/code]`)
+
+**Version 0.17**
+
+* Fixed a bug in the Bash highlighting code which caused conflicts between keywords and variables highlights
+
+**Version 0.16**
+
+* Ported SyntaxHighlighter 1.1.1 changes: 'Encode single quotes so `wptexturize()` doesn't transform them into fancy quotes and screw up code.'
+
+**Version 0.15b**
+
+* CSS fixes for IE6
+
+**Version 0.15a**
+
+* enhanced CSS
+
+**Version 0.15**
+
+* Fixed the IE scroll bar issue (backup your CSS file before upgrade!)
+
+**Version 0.14**
+
+* Added support for Bash (thanks to [Nick Anderson](http://www.cmdln.org/2008/04/07/syntaxhighlighter-plus-patch/))
+* Now supports Wordpress MU (thanks to [Tim](http://tim.diary.tw/2008/03/05/syntaxhighlighter-plus/))
+
+**Version 0.13**
+
+* Changed plugin folder to 'syntaxhighlighter-plus'.
+
+**Version 0.12 (broken)**
+
+* Appeared on the Wordpress plugins site.
+* Fixed readme.txt.
+* Cleaned up the files.
+
+**Version 0.11**
+
+* Added more syntax variants.
+
+**Version 0.10**
+
+* Initial release!
---
+++ syntaxhighlighter.php
@@ -0,0 +1,424 @@
+<?php /*
+
+**************************************************************************
+
+Plugin Name:      SyntaxHighlighter Plus
+Plugin URI:       http://thislab.com/2007/12/16/release-wordpress-plugin-syntaxhighlighter-plus/
+Version:          1.0b1
+Description:      An advanced upload-and-activate WordPress implementation of Alex Gorbatchev's <a href="http://code.google.com/p/syntaxhighlighter/">SyntaxHighlighter</a> JavaScript code highlighting package. See WordPress.com's "<a href="http://faq.wordpress.com/2007/09/03/how-do-i-post-source-code/">How do I post source code?</a>" for details. <strong><a href="options-general.php?page=syntaxhighlighter-plus/syntaxhighlighter.php">Click here for options</a></strong>.
+Author:           <a href="http://thislab.com/">Fred Wu</a>
+Original Authors: <a href="http://photomatt.net/">Matt</a>, <a href="http://www.viper007bond.com/">Viper007Bond</a>, and <a href="http://blogwaffe.com/">mdawaffe</a>
+
+**************************************************************************
+
+Credits:
+
+* Matt ( photomatt.net ) -- original concept and code on WP.com
+* Viper007Bond ( viper007bond.com ) -- current plugin version
+
+Simply put, Matt deserves the majority of the credit for this plugin.
+I (Viper007Bond) just took the plugin I had already written (it looked a
+lot like this current one) after seeing his code operate on WP.com and
+incorporated his ingenius TinyMCE handling and some other misc. code.
+
+**************************************************************************/
+
+class AGSyntaxHighlighter {
+	var $languages = array();
+	var $languagesregex;
+	var $jsfiles2load = array();
+	var $pluginurl;
+	var $kses_active = array();
+	var $kses_filters = array();
+	var $widget_format_to_edit = false;
+	var $default_language = 'php';
+
+	// WordPress hooks
+	function AGSyntaxHighlighter() {
+		add_action( 'init', array(&$this, 'SetVariables'), 1000 );
+		add_action( 'wp_head', array(&$this, 'AddStylesheet'), 1000 );
+		add_action( 'admin_head', array(&$this, 'AddStylesheet'), 1000 );
+		add_action( 'wp_footer', array(&$this, 'FileLoader'), 1000 );
+		add_action( 'admin_footer', array(&$this, 'FileLoader'), 1000 ); // For viewing comments in admin area
+		
+		// admin menu
+		add_action( 'admin_menu', array(&$this, 'syntaxhighlighterplus_admin_menu') );
+		
+		// Find and replace the BBCode
+		add_filter( 'the_content', array(&$this, 'BBCodeToHTML'), 8 );
+		add_filter( 'widget_text', array(&$this, 'BBCodeToHTML'), 8 );
+
+		// Account for kses
+		add_filter( 'content_save_pre', array(&$this, 'before_kses_normalization'), 1 );
+		add_filter( 'content_save_pre', array(&$this, 'after_kses_normalization'), 11 );
+		add_action( 'admin_head', array(&$this, 'before_kses_normalization_widget'), 1 );
+		add_action( 'update_option_widget_text', array(&$this, 'after_kses_normalization_widget'), 1, 2 );
+		add_filter( 'format_to_edit', array(&$this, 'after_kses_normalization_widget_format_to_edit'), 1 );
+
+		// Account for TinyMCE
+		add_filter( 'content_save_pre', array(&$this, 'TinyMCEDecode'), 8 );
+		add_filter( 'the_editor_content', array(&$this, 'TinyMCEEncode'), 8 );
+
+		// Uncomment these next lines to allow commenters to post code
+		//add_filter( 'comment_text', array(&$this, 'BBCodeToHTML'), 8 );
+		//add_filter( 'pre_comment_content', array(&$this, 'before_kses_normalization_comment'), 1 );
+		//add_filter( 'pre_comment_content', array(&$this, 'after_kses_normalization_comment'), 11 );
+	}
+	
+	function syntaxhighlighterplus_admin_menu()
+	{
+		if (function_exists('add_options_page'))
+		{
+			add_options_page(__('SyntaxHighlighter Plus Configuration'), __('SyntaxHighlighter Plus'), 8, __FILE__, array(&$this, 'syntaxhighlighterplus_config'));
+		}
+	}
+	
+	function syntaxhighlighterplus_config()
+	{
+		$themes = scandir(ABSPATH . PLUGINDIR . '/syntaxhighlighter-plus/syntaxhighlighter/styles/');
+		$options = get_option('syntaxhighlighterplus_options');
+		
+		if (isset($_POST['submit']))
+		{
+			$options['theme'] = $_POST['theme'];
+			update_option('syntaxhighlighterplus_options', $options);
+		}
+		
+		$o = '<div class="wrap">'."\n";
+		$o .= '<h2>'.__('SyntaxHighlighter Plus Configuration').'</h2>'."\n";
+		$o .= '<form action="options-general.php?page=syntaxhighlighter-plus/syntaxhighlighter.php" method="post" accept-charset="utf-8">'."\n";
+		$o .= '<h3>Choose a theme</h3>'."\n";
+		$o .= '<select name="theme" id="theme">'."\n";
+		
+		foreach ($themes as $theme)
+		{
+			if (substr($theme, -3) == 'css' && $theme != 'shCore.css')
+			{
+				$selected = $theme == $options['theme'] ? 'selected="selected"' : '';
+				
+				$o .= '	<option value="'.$theme.'" '.$selected.'>'.__($theme).'</option>'."\n";
+			}
+		}
+		
+		$o .= '</select>&nbsp;&nbsp;<a href="http://alexgorbatchev.com/wiki/SyntaxHighlighter:Themes" target="_blank">Theme Previews</a>'."\n";
+		$o .= '<p class="submit"><input type="submit" name="submit" value="'.__('Update Settings').' &raquo;" /></p>'."\n";
+		$o .= '</form>'."\n";
+		$o .= '</div>'."\n";
+		
+		echo $o;
+	}
+
+	// Set some variables now that we've given all other plugins a chance to load
+	function SetVariables() {
+		$this->pluginurl = apply_filters( 'agsyntaxhighlighter_url', get_bloginfo( 'wpurl' ) . '/wp-content/plugins/syntaxhighlighter-plus/' );
+
+		// Define all allowed languages and allow plugins to modify this
+		$this->languages = apply_filters( 'agsyntaxhighlighter_languages', array(
+			'bash'       => 'shBrushBash.js',
+			'sh'         => 'shBrushBash.js',
+			'cpp'        => 'shBrushCpp.js',
+			'c'          => 'shBrushCpp.js',
+			'c++'        => 'shBrushCpp.js',
+			'c#'         => 'shBrushCSharp.js',
+			'c-sharp'    => 'shBrushCSharp.js',
+			'csharp'     => 'shBrushCSharp.js',
+			'css'        => 'shBrushCss.js',
+			'delphi'     => 'shBrushDelphi.js',
+			'pascal'     => 'shBrushDelphi.js',
+			'diff'       => 'shBrushDiff.js',
+			'groovy'     => 'shBrushGroovy.js',
+			'java'       => 'shBrushJava.js',
+			'js'         => 'shBrushJScript.js',
+			'jscript'    => 'shBrushJScript.js',
+			'javascript' => 'shBrushJScript.js',
+			'php'        => 'shBrushPhp.js',
+			'plain'      => 'shBrushPlain.js',
+			'text'       => 'shBrushPlain.js',
+			''           => 'shBrushPlain.js',
+			'py'         => 'shBrushPython.js',
+			'python'     => 'shBrushPython.js',
+			'rb'         => 'shBrushRuby.js',
+			'ruby'       => 'shBrushRuby.js',
+			'rails'      => 'shBrushRuby.js',
+			'ror'        => 'shBrushRuby.js',
+			'sql'        => 'shBrushSql.js',
+			'vb'         => 'shBrushVb.js',
+			'vb.net'     => 'shBrushVb.js',
+			'xml'        => 'shBrushXml.js',
+			'html'       => 'shBrushXml.js',
+			'xhtml'      => 'shBrushXml.js',
+			'xslt'       => 'shBrushXml.js',
+		) );
+
+		// Quote them to make them regex safe
+		$languages = array();
+		foreach ( $this->languages as $language => $filename ) $languages[] = preg_quote( $language );
+
+		// Generate the regex for them
+		$this->languagesregex = '(' . implode( '|', $languages ) . ')';
+
+		$this->kses_filters = apply_filters( 'agsyntaxhighlighter_kses_filters', array(
+			'wp_filter_kses',
+			'wp_filter_post_kses',
+			'wp_filter_nohtml_kses'
+		) );
+	}
+
+
+	// We need to stick the stylesheet in the header for best results
+	function AddStylesheet() {
+		echo '	<link type="text/css" rel="stylesheet" href="' . $this->pluginurl . 'syntaxhighlighter/styles/shCore.css"></link>' . "\n";
+		
+		$options = get_option('syntaxhighlighterplus_options');
+		$options['theme'] = isset($options['theme']) ? $options['theme'] : 'shThemeDefault.css';
+		
+		echo '	<link type="text/css" rel="stylesheet" href="' . $this->pluginurl . 'syntaxhighlighter/styles/'.$options['theme'].'"></link>' . "\n";
+	}
+
+
+	// This function checks for the BBCode cheaply so we don't waste CPU cycles on regex if it's not needed
+	// It's in a seperate function since it's used in mulitple places (makes it easier to edit)
+	function CheckForBBCode( $content ) {
+		if ( stristr( $content, '[sourcecode' ) && stristr( $content, '[/sourcecode]' ) ) return TRUE;
+		if ( stristr( $content, '[source' ) && stristr( $content, '[/source]' ) ) return TRUE;
+		if ( stristr( $content, '[code' ) && stristr( $content, '[/code]' ) ) return TRUE;
+		if ( stristr( $content, '[lang' ) && stristr( $content, '[/lang]' ) ) return TRUE;
+		
+		if ( stristr( $content, '[bash' ) && stristr( $content, '[/bash]' ) ) return TRUE;
+		if ( stristr( $content, '[sh' ) && stristr( $content, '[/sh]' ) ) return TRUE;
+		if ( stristr( $content, '[cpp' ) && stristr( $content, '[/cpp]' ) ) return TRUE;
+		if ( stristr( $content, '[c++' ) && stristr( $content, '[/c++]' ) ) return TRUE;
+		if ( stristr( $content, '[c#' ) && stristr( $content, '[/c#]' ) ) return TRUE;
+		if ( stristr( $content, '[c-sharp' ) && stristr( $content, '[/c-sharp]' ) ) return TRUE;
+		if ( stristr( $content, '[csharp' ) && stristr( $content, '[/csharp]' ) ) return TRUE;
+		if ( stristr( $content, '[css' ) && stristr( $content, '[/css]' ) ) return TRUE;
+		if ( stristr( $content, '[delphi' ) && stristr( $content, '[/delphi]' ) ) return TRUE;
+		if ( stristr( $content, '[pascal' ) && stristr( $content, '[/pascal]' ) ) return TRUE;
+		if ( stristr( $content, '[diff' ) && stristr( $content, '[/diff]' ) ) return TRUE;
+		if ( stristr( $content, '[groovy' ) && stristr( $content, '[/groovy]' ) ) return TRUE;
+		if ( stristr( $content, '[java' ) && stristr( $content, '[/java]' ) ) return TRUE;
+		if ( stristr( $content, '[js' ) && stristr( $content, '[/js]' ) ) return TRUE;
+		if ( stristr( $content, '[jscript' ) && stristr( $content, '[/jscript]' ) ) return TRUE;
+		if ( stristr( $content, '[javascript' ) && stristr( $content, '[/javascript]' ) ) return TRUE;
+		if ( stristr( $content, '[php' ) && stristr( $content, '[/php]' ) ) return TRUE;
+		if ( stristr( $content, '[plain' ) && stristr( $content, '[/plain]' ) ) return TRUE;
+		if ( stristr( $content, '[text' ) && stristr( $content, '[/text]' ) ) return TRUE;
+		if ( stristr( $content, '[py' ) && stristr( $content, '[/py]' ) ) return TRUE;
+		if ( stristr( $content, '[python' ) && stristr( $content, '[/python]' ) ) return TRUE;
+		if ( stristr( $content, '[rb' ) && stristr( $content, '[/rb]' ) ) return TRUE;
+		if ( stristr( $content, '[ruby' ) && stristr( $content, '[/ruby]' ) ) return TRUE;
+		if ( stristr( $content, '[rails' ) && stristr( $content, '[/rails]' ) ) return TRUE;
+		if ( stristr( $content, '[ror' ) && stristr( $content, '[/ror]' ) ) return TRUE;
+		if ( stristr( $content, '[sql' ) && stristr( $content, '[/sql]' ) ) return TRUE;
+		if ( stristr( $content, '[vb' ) && stristr( $content, '[/vb]' ) ) return TRUE;
+		if ( stristr( $content, '[vb.net' ) && stristr( $content, '[/vb.net]' ) ) return TRUE;
+		if ( stristr( $content, '[xml' ) && stristr( $content, '[/xml]' ) ) return TRUE;
+		if ( stristr( $content, '[html' ) && stristr( $content, '[/html]' ) ) return TRUE;
+		if ( stristr( $content, '[xhtml' ) && stristr( $content, '[/xhtml]' ) ) return TRUE;
+		if ( stristr( $content, '[xslt' ) && stristr( $content, '[/xslt]' ) ) return TRUE;
+
+		return FALSE;
+	}
+
+
+	// This function is a wrapper for preg_match_all() that grabs all BBCode calls
+	// It's in a seperate function since it's used in mulitple places (makes it easier to edit)
+	function GetBBCode( $content, $addslashes = FALSE ) {
+		$regex = '/\[(sourcecode|source|code|lang|)( language=| lang=|=|)';
+		if ( $addslashes ) $regex .= '\\\\';
+		$regex .= '([\'"]|)' . $this->languagesregex;
+		if ( $addslashes ) $regex .= '\\\\';
+		$regex .= '\3\](.*?)\[\/(\1|\4)\]/si';
+
+		preg_match_all( $regex, $content, $matches, PREG_SET_ORDER );
+		
+		return $matches;
+	}
+
+
+	/* If KSES is going to hit this text, we double encode stuff within the [sourcecode] tags to keep 
+	 * 	wp_kses_normalize_entities from breaking them.
+	 * $content = text to parse
+	 * $which_filter = which filter to check to see if kses will be applied
+	 * $addslashes = used by AGSyntaxHighlighter::GetBBCode
+	 */
+	function before_kses_normalization( $content, $which_filter = 'content_save_pre', $addslashes = true ) {
+		global $wp_filter;
+		if ( is_string($which_filter) && !isset($this->kses_active[$which_filter]) ) {
+			$this->kses_active[$which_filter] = false;
+			$filters = $wp_filter[$which_filter];
+			foreach ( (array) $filters as $priority => $filter ) {
+				foreach ( $filter as $k => $v ) {
+					if ( in_array( $filter[$k]['function'], $this->kses_filters ) ) {
+						$this->kses_active[$which_filter] = true;
+						break 2;
+					}
+				}
+			}
+		}
+
+		if ( ( true === $which_filter || $this->kses_active[$which_filter] ) && $this->CheckForBBCode( $content ) ) {
+			$matches = $this->GetBBCode( $content, $addslashes );
+			foreach( (array) $matches as $match )
+				$content = str_replace( $match[5], htmlspecialchars( $match[5], ENT_QUOTES ), $content );
+		}
+		return $content;
+	}
+
+
+	/* We undouble encode the stuff within [sourcecode] tags to fix the output of
+	 * 	AGSyntaxHighlighter::before_kses_normalization.
+	 */
+	function after_kses_normalization( $content, $which_filter = 'content_save_pre', $addslashes = true ) {
+		if ( ( true === $which_filter || $this->kses_active[$which_filter] ) && $this->CheckForBBCode( $content ) ) {
+			$matches = $this->GetBBCode( $content, $addslashes );
+			foreach( (array) $matches as $match )
+				$content = str_replace( $match[5], htmlspecialchars_decode( $match[5], ENT_QUOTES ), $content );
+		}
+		return $content;
+	}
+
+
+	// Wrapper for comment text
+	function before_kses_normalization_comment( $content ) {
+		return $this->before_kses_normalization( $content, 'pre_comment_content' );
+	}
+
+
+	function after_kses_normalization_comment( $content ) {
+		return $this->after_kses_normalization( $content, 'pre_comment_content' );
+	}
+
+
+	/* "Wrapper" for widget text.  Since we lack the necessary filters, we directly alter the
+	 * 	submitted $_POST variables before the widgets are updated.
+	 */
+	function before_kses_normalization_widget() {
+		global $pagenow;
+		if ( 'widgets.php' != $pagenow || current_user_can( 'unfiltered_html' ) )
+			return;
+
+		$i = 1;
+		while ( isset($_POST["text-submit-$i"]) ) {
+			$_POST["text-text-$i"] = $this->before_kses_normalization( $_POST["text-text-$i"], true );
+			$i++;
+		}
+	}
+
+	// Again, since we lack the needed filters, we have to check the freshly updated option and re-update it.
+	function after_kses_normalization_widget( $old, $new ) {
+		static $do_update = true;
+
+		if ( !$do_update || current_user_can( 'unfiltered_html' ) )
+			return;
+
+		foreach ( array_keys($new) as $i => $widget )
+			$new[$i]['text'] = $this->after_kses_normalization( $new[$i]['text'], true, false );
+
+		$do_update = false;
+
+		update_option( 'widget_text', $new );
+		$this->widget_format_to_edit = true;
+
+		$do_update = true;
+	}
+
+	// Totally lame.  The output of the widget form in the admin screen is cached from before our re-update.
+	function after_kses_normalization_widget_format_to_edit( $content ) {
+		if ( !$this->widget_format_to_edit )
+			return $content;
+
+		$content = $this->after_kses_normalization( $content, true, false );
+
+		$this->widget_format_to_edit = false;
+
+		return $content;
+	}
+
+	// Reverse changes TinyMCE made to the entered code
+	function TinyMCEDecode( $content ) {
+		if ( !user_can_richedit() || !$this->CheckForBBCode( $content ) ) return $content;
+
+		// Find all BBCode (remember, it's all slash escaped!)
+		$matches = $this->GetBBCode( $content, TRUE );
+
+		if ( empty($matches) ) return $content; // No BBCode found, we can stop here
+
+		// Loop through each match and decode the code
+		foreach ( (array) $matches as $match ) {
+			$content = str_replace( $match[5], htmlspecialchars_decode( $match[5] ), $content );
+		}
+
+		return $content;
+	}
+
+
+	// (Re)Encode the code so TinyMCE will display it correctly
+	function TinyMCEEncode( $content ) {
+		if ( !user_can_richedit() || !$this->CheckForBBCode( $content ) ) return $content;
+
+		$matches = $this->GetBBCode( $content );
+
+		if ( empty($matches) ) return $content; // No BBCode found, we can stop here
+
+		// Loop through each match and encode the code
+		foreach ( (array) $matches as $match ) {
+			$code = htmlspecialchars( $match[5] );
+			$code = str_replace( '&amp;', '&amp;amp;', $code );
+			$code = str_replace( '&amp;lt;', '&amp;amp;lt;', $code );
+			$code = str_replace( '&amp;gt;', '&amp;amp;gt;', $code );
+
+			$content = str_replace( $match[5], $code, $content );
+		}
+
+		return $content;
+	}
+
+
+	// The meat of the plugin. Find all valid BBCode calls and replace them with HTML for the Javascript to handle.
+	function BBCodeToHTML( $content ) {
+		if ( !$this->CheckForBBCode( $content ) ) return $content;
+
+		$matches = $this->GetBBCode( $content );
+
+		if ( empty($matches) ) return $content; // No BBCode found, we can stop here
+
+		// Loop through each match and replace the BBCode with HTML
+		foreach ( (array) $matches as $match ) {
+			$language = $match[4] == '' ? $this->default_language : strtolower( $match[4] );
+			$content = str_replace( $match[0], '<pre class="brush: ' . $language . "\">" . htmlspecialchars( $match[5], ENT_QUOTES ) . "</pre>", $content );
+			$this->jsfiles2load[$this->languages[$language]] = TRUE;
+		}
+		
+		return $content;
+	}
+
+
+	// Output the HTML to load all of SyntaxHighlighter's Javascript, CSS, and SWF files
+	function FileLoader() {
+		?>
+
+<!-- SyntaxHighlighter Stuff -->
+<script type="text/javascript" src="<?php echo $this->pluginurl; ?>syntaxhighlighter/src/shCore.js"></script>
+<?php foreach ( $this->jsfiles2load as $filename => $foobar ) : ?>
+<script type="text/javascript" src="<?php echo $this->pluginurl . 'syntaxhighlighter/scripts/' . $filename; ?>"></script>
+<?php endforeach; ?>
+<script type="text/javascript">
+	SyntaxHighlighter.all();
+</script>
+
+<?php
+	}
+}
+
+// Initiate the plugin class
+$AGSyntaxHighlighter = new AGSyntaxHighlighter();
+
+// For those poor souls stuck on PHP4
+if ( !function_exists( 'htmlspecialchars_decode' ) ) {
+	function htmlspecialchars_decode( $string, $quote_style = ENT_COMPAT ) {
+		return strtr( $string, array_flip( get_html_translation_table( HTML_SPECIALCHARS, $quote_style) ) );
+	}
+}
+
+?>
